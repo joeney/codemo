bf590692fd6898a4f18b25c62e7abaef8e7adfc2
andy <andy@dd>
2015-06-08 18:01:07 +0800

    init

---
+++ common/src/main/java/common/App.java
@@ -0,0 +1,16 @@
+package common;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	private static final Log logger = LogFactory.getLog(App.class);
+
+	public static void main(String[] args) {
+		logger.info("Hello World!");
+	}
+}
